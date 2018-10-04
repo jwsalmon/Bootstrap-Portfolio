244943113f1f93557990a5dfe0b34b125cbc4189
Jesse W. Salmon <j_aw_s@icloud.com>
2018-10-03 18:07:03 -0700

    add StarWarsRPG and BandSearcher project links to protfolio

---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,11 @@
+function launchHG(){
+    window.open("https://jwsalmon.github.io/Word-Guess-Game/");
+}
+
+function launchRPG(){
+    window.open("https://jwsalmon.github.io/unit-4-game/")
+}
+
+function launchBS(){
+    window.open("https://jwsalmon.github.io/bandSearcher/")
+}
